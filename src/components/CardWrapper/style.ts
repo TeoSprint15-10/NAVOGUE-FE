fc24ba67938322fc1386a668d0483452309aa426
jeongdopark <jdpark0816@gmail.com>
2023-06-25 00:19:42 +0900

    style:  height

--- src/components/CardWrapper/style.ts
+++ src/components/CardWrapper/style.ts
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const CardWrapper = styled.li`
   background-color: white;
   border-radius: 15px;
+  height: 200px;
 `;
--- src/pages/MainPage/style.ts
+++ src/pages/MainPage/style.ts
@@ -4,7 +4,6 @@ const Container = styled.div`
   position: relative;
   top: 25%;
   width: 100%;
-  height: 100vh;
   background-color: salmon;
   display: flex;
 `;
@@ -13,6 +12,7 @@ const MainContent = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+
   margin-left: 50px;
 `;
 
