389168460c280d73a7bec0c4b9079ced49c7785b
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-06-24 19:48:41 +0900

    style: 헤더 레이아웃 작업

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
 import * as S from "./style";
+const searchIconUrl = new URL("/searchIcon.png", import.meta.url).href;
+const logoImgUrl = new URL("/logo.svg", import.meta.url).href;
+const logoTextImgUrl = new URL("/NAVOGUE.png", import.meta.url).href;
 
 export default function Header() {
   return (
     <S.Wrapper>
-      <S.Overlay></S.Overlay>
-      <S.Box>Header</S.Box>
+      <S.Overlay />
+      <S.Box>
+        <S.Logo>
+          <S.LogoImg src={logoImgUrl} alt="Logo" />
+          <S.LogoTextImg src={logoTextImgUrl} alt="NAVOGUE" />
+        </S.Logo>
+        <S.SearchInput></S.SearchInput>
+        <S.SearchBtn>
+          <S.SearchIcon src={searchIconUrl} alt="Search Icon" />
+        </S.SearchBtn>
+      </S.Box>
     </S.Wrapper>
   );
 }
