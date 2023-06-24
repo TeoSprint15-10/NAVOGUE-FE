df5bfd14cf73051cad6be014e5014394ad6ca486
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-06-24 22:17:05 +0900

    chore: axios 설치

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,11 @@
-export {};
+export type TextMemo = {
+  id: number;
+  type: string;
+  content: string;
+  isPinned: boolean;
+  tags: Tag[];
+};
+export type Tag = {
+  id: number;
+  name: string;
+};
