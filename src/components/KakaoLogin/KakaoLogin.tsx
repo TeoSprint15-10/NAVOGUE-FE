602a09a0834035c401e69137a388aca2eb0a8105
solssak <gswoong1012@gmail.com>
2023-06-25 04:25:44 +0900

    style: 로그인 페이지 스타일 작업

--- src/components/KakaoLogin/KakaoLogin.tsx
+++ src/components/KakaoLogin/KakaoLogin.tsx
@@ -1,4 +1,6 @@
 import { S } from "./style";
+import logoImgUrl from "../../../public/assets/LOGIN_LOGO.png";
+import KakaoLoginImgUrl from "../../../public/assets/KAKAO_LOGIN.png";
 
 const KakaoLogin = () => {
   const Rest_api_key = import.meta.env.VITE_APP_KAKAO_CLIENT_ID;
@@ -7,11 +9,12 @@ const KakaoLogin = () => {
 
   return (
     <S.LoginBox>
-      {/* <S.Slogan>내 메모를 스마트하게 태깅</S.Slogan> */}
+      <S.Slogan>내 메모를 스마트하게 태깅</S.Slogan>
+      <S.LogoImg src={logoImgUrl} alt="LogoImg"></S.LogoImg>
       <S.KakaoLoginBtn>
-        <S.Text onClick={() => (window.location.href = KAKAO_AUTH_URL)}>
-          카카오 계정으로 로그인
-        </S.Text>
+        <S.Text onClick={() => (window.location.href = KAKAO_AUTH_URL)} />
+        <S.KakaoImg src={KakaoLoginImgUrl} alt="KakaoImg"></S.KakaoImg>
+        카카오 계정으로 로그인
       </S.KakaoLoginBtn>
     </S.LoginBox>
   );
--- src/components/KakaoLogin/style.ts
+++ src/components/KakaoLogin/style.ts
@@ -2,18 +2,21 @@ import { styled } from "styled-components";
 
 const LoginBox = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  flex-diretion: column;
+  flex-direction: column;
+  justify-content: space-between; /* Added this line to evenly distribute space */
+  align-items: center;
   border: 5px solid #a0c9a2;
-  width: 100%;
-  height: 100%;
+  width: 600px;
+  height: 700px;
   border-radius: 70px;
+  margin: 200px auto;
 `;
 
 const Slogan = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center; /* Added this line to center vertically */
+  margin-top: 20px; /* Adjust this value for desired spacing */
 `;
 
 const KakaoLoginBtn = styled.div`
@@ -23,15 +26,31 @@ const KakaoLoginBtn = styled.div`
   background-color: white;
   width: 237px;
   height: 49px;
-  margin-top: 20px;
+  margin-top: 50px;
   margin-bottom: 30px;
   border: 2px solid #f0c23a;
   border-radius: 50px;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  margin: 20px auto;
 `;
 
 const Text = styled.span`
   text-align: center;
 `;
 
-export const S = { LoginBox, Slogan, KakaoLoginBtn, Text };
+const LogoImg = styled.img`
+  width: 200px;
+  height: 250px;
+  margin-bottom: 100px;
+`;
+
+const KakaoImg = styled.img`
+  width: 22px;
+  height: 22px;
+  margin-right: 10px;
+`;
+
+export const logoImgUrl = styled.img``;
+export const KakaoLoginImgUrl = styled.img``;
+
+export const S = { LoginBox, Slogan, KakaoLoginBtn, Text, LogoImg, KakaoImg };
