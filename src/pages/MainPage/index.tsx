3048124225eb725f8e5164ddf9b16bf36853d34b
jeongdopark <jdpark0816@gmail.com>
2023-06-24 22:54:11 +0900

    chore: 불필요한 요소 삭제 및 컴포넌트명 변경

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,12 +1,12 @@
 import { S } from "./style";
 import Sidebar from "../../components/Sidebar";
-import MemoBox from "../../components/MemoBox";
+import MemoInputBox from "../../components/MemoInputBox";
 export default function MainPage() {
   return (
     <S.Container>
       <Sidebar />
       <S.MainContent>
-        <MemoBox />
+        <MemoInputBox />
       </S.MainContent>
     </S.Container>
   );
