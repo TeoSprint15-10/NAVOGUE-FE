a29198e0dcdecae543c313924c867c795ea34836
solssak <gswoong1012@gmail.com>
2023-06-26 03:41:04 +0900

    feat: 카카오 간편 로그인 구현

--- src/components/KakaoCallback/KakaoCallback.tsx
+++ src/components/KakaoCallback/KakaoCallback.tsx
@@ -3,44 +3,29 @@ import axios from "axios";
 
 const KakaoCallback = () => {
   useEffect(() => {
+    // const grant_type = "authorization_code";
+    // const Rest_api_key = import.meta.env.VITE_APP_KAKAO_CLIENT_ID;
+    // const redirect_uri = import.meta.env.VITE_APP_KAKAO_REDIRECT_URI;
     const params = new URL(document.location.toString()).searchParams;
     const code = params.get("code");
-    const grant_type = "authorization_code";
-    const Rest_api_key = import.meta.env.VITE_APP_KAKAO_CLIENT_ID;
-    const redirect_uri = import.meta.env.VITE_APP_KAKAO_REDIRECT_URI;
+    const BASE_URL = "http://courtney.reverof.p-e.kr:8080";
 
     axios
-      .post(
-        `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&code=${code}`,
-        {},
-        {
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-          },
-        }
-      )
+      .get(`${BASE_URL}/user/login?code=${code}`)
       .then((res) => {
-        console.log(res);
-        // 백엔드 서버에 access_token 보내주는 로직
-        // const { access_token } = res.data;
-        // axios
-        //   .post(
-        //     `https://kapi.kakao.com/v2/user/me`,
-        //     {},
-        //     {
-        //       headers: {
-        //         Authorization: `Bearer ${access_token}`,
-        //         "Content-type":
-        //           "application/x-www-form-urlencoded;charset=utf-8",
-        //       },
-        //     }
-        //   )
+        console.log(res.data.accessToken);
+        const data = res.data.accessToken;
+        localStorage.setItem("token", JSON.stringify(data));
+        window.location.href = "http://localhost:5173";
+      })
+      .catch((error) => {
+        console.error("API 요청 에러", error.response);
       });
-    // .catch((Error: any) => {
-    //   console.log("Error", Error);
-    // });
-    // .
   }, []);
+
+  const removeToken = () => {
+    localStorage.removeItem("token");
+  };
 
   return (
     <>
