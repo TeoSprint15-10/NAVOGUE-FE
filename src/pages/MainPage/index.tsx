345bdaee739d839f189a4b4c6dc09cad0122b5c0
nahyyun <nahyuun@naver.com>
2023-06-26 02:48:20 +0900

    feat: page param 초기 값 수정

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -9,9 +9,9 @@ import { getMemoList } from "../../api/memo";
 import { useIntersectionObserver } from "../../hooks/useIntersectionObeserver";
 
 export default function MainPage() {
-  const { data, fetchNextPage, isLoading, isError } = useInfiniteQuery({
+  const { data, fetchNextPage } = useInfiniteQuery({
     queryKey: [QUERY_KEY.MEMO_LIST],
-    queryFn: ({ pageParam = 1 }) => getMemoList(pageParam),
+    queryFn: ({ pageParam = 0 }) => getMemoList(pageParam),
     getNextPageParam: (lastPage) =>
       lastPage.last ? undefined : lastPage.pageable.pageNumber + 1,
 
