efd5ad546179bdba2192b5781647ec0a9c532f1c
jeongdopark <jdpark0816@gmail.com>
2023-06-25 16:06:38 +0900

    feat: type filter custom hook

---
+++ src/hooks/useContentTypeFilter.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+import { getMemoSearchedList } from "../api/memo";
+// 컨텐츠 타입에 따른 필터링
+
+const useContentTypeFilter = () => {
+  const [crntFilterState, setCrntFilterState] = useState([false, false, false]);
+  const FILTER_TYPE = ["ALL", "URL", "TEXT"];
+  const FILTER_TYPE_IDX = useRef<number>(0);
+  const onClickType = (id: number) => {
+    setCrntFilterState((prevState) => {
+      const updatedFilterState = prevState.map((state, index) => {
+        return index === id ? true : false;
+      });
+      return updatedFilterState;
+    });
+  };
+
+  useEffect(() => {
+    FILTER_TYPE_IDX.current = crntFilterState.indexOf(true);
+    if (FILTER_TYPE_IDX.current === 0) {
+      const getMemoData = getMemoSearchedList();
+    } else {
+      const getMemoData = getMemoSearchedList("keyword", `${FILTER_TYPE[FILTER_TYPE_IDX.current]}`);
+    }
+  }, [crntFilterState]);
+
+  return { crntFilterState, onClickType };
+};
+
+export default useContentTypeFilter;
