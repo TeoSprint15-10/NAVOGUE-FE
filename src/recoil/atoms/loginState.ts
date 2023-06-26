78fc5a5a044a4c93a46f38f0112d9066fc60a5cf
nahyyun <nahyuun@naver.com>
2023-06-26 21:27:25 +0900

    fix: token parse 작업 수정

--- src/recoil/atoms/loginState.ts
+++ src/recoil/atoms/loginState.ts
@@ -4,5 +4,5 @@ const token = localStorage.getItem("token") || "";
 
 export const loginState = atom({
   key: "isLogin",
-  default: JSON.parse(token) ? true : false,
+  default: token ? true : false,
 });
