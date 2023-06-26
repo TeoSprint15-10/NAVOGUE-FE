76bf7a315fe0db78f7beca3e669fea20417db60c
nahyyun <nahyuun@naver.com>
2023-06-26 19:18:44 +0900

    fix: redirect url 수정

--- src/components/KakaoLogin/KakaoLogin.tsx
+++ src/components/KakaoLogin/KakaoLogin.tsx
@@ -4,7 +4,7 @@ import KakaoLoginImgUrl from "../../../public/assets/KAKAO_LOGIN.png";
 
 const KakaoLogin = () => {
   const Rest_api_key = import.meta.env.VITE_APP_KAKAO_CLIENT_ID;
-  const redirect_uri = "https://main--gleeful-kulfi-17f59b.netlify.app/";
+  const redirect_uri = "https://main--gleeful-kulfi-17f59b.netlify.app";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
   return (
